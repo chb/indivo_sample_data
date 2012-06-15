d5042d94b0e770970ffddbfbaaab52ea46b5e76a
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-06-15 21:10:44 +0000

    Updated analyze script to work with new data models

--- analyze_sample_patients.sh
+++ analyze_sample_patients.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-
-for d in `ls`; do echo "$d: `grep -r 'Lab>' $d | wc -l` Labs, `grep -r 'Problem>' $d | wc -l` Problems, `grep -r 'Medication>' $d | wc -l` Medications, `grep -r 'Immunization>' $d | wc -l` Immunizations, `grep -r 'Allergy>' $d | wc -l` Allergies, `grep -r 'VitalSign>' $d | wc -l` Vitals"; done
+for d in `ls --hide *.sh`; do echo "$d: `grep -r 'name=\"LabResult' $d | wc -l` Labs, `grep -r 'name=\"Problem' $d | wc -l` Problems, `grep -r 'name=\"Medication' $d | wc -l` Medications, `grep -r 'name=\"Fill\"' $d | wc -l` Fills, `grep -r 'name=\"Immunization' $d | wc -l` Immunizations, `grep -r 'name=\"Allergy\"' $d | wc -l` Allergies, `grep -r 'name=\"AllergyExclusion' $d | wc -l` Allergy Exclusions, `grep -r 'name=\"VitalSign' $d | wc -l` Vitals"; done
